0aaa1d3c26a02aa75a90ddffe135cea344e6ff8b
Puvii-1 <purvipati45@gmail.com>
2025-10-07 10:35:23 +0530

    operator-practice

---
+++ day-07/10-js/main.js
@@ -0,0 +1,31 @@
+
+// //ternary operator
+let num = 11;
+num % 2==0? console.log("even number"): console.log("odd number")
+
+let age = 16;
+age > 18? console.log("You can vote."): console.log("You cannot vote.")
+
+let num1 = -134;
+let message = num1 >= 0 ? "Number is positive": "Number is negative";
+console.log(message);
+
+
+//Decision Making Statements
+let time = "morning";
+
+if (time ==="morning"){
+    console.log("Good Morning");
+} else{
+    console.log("Good Afternoon");
+}
+
+
+//last digit even or odd
+let num3 = 2121;
+
+if(num3 % 2==0){
+    console.log("Even");
+}else{
+    console.log("Odd");
+}
