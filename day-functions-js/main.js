1c1cd205294da10fd16cd0074d86a74a534538e6
Puvii-1 <purvipati45@gmail.com>
2025-10-10 23:05:14 +0530

    functions-half

---
+++ day-functions-js/main.js
@@ -0,0 +1,25 @@
+// commands to buy milk from a store
+function getMilk(){  
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+getMilk();
+
+//Parameters
+
